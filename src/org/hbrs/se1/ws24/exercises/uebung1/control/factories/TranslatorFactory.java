2561cf22020af68a9d83ba717a5cadf657b7d2fd
tobHBRs <tobias.schomacker2@web.de>
2024-10-15 10:06:21 +0200

    Ü1 in GIT integriert

--- src/org/hbrs/se1/ws24/exercises/uebung1/control/factories/TranslatorFactory.java
+++
@@ -1,4 +0,0 @@
-package org.hbrs.se1.ws24.exercises.uebung1.control.factories;
-
-public class TranslatorFactory {
-}
--- test/org/hbrs/se1/ws24/tests/uebung1/GermanTranslatorTest.java
+++ test/org/hbrs/se1/ws24/tests/uebung1/GermanTranslatorTest.java
@@ -6,10 +6,29 @@ import org.junit.jupiter.api.Test;
 public class GermanTranslatorTest {
 
     @Test
-    public void aTest() {
+    public void NullTest() { //Null als Grenzwert testen
         GermanTranslator translator = new GermanTranslator();
 
-        assertEquals("null" , translator.translateNumber(0));
+        assertEquals("Übersetzung der Zahl 0 nicht möglich (1.0)" , translator.translateNumber(0));
     }
+    @Test
+    public void Pos1Test() { //Positive Zahlen im Definitionsbereich
+        GermanTranslator translator = new GermanTranslator();
+
+        assertEquals("fünf" , translator.translateNumber(5));
+    }
+    @Test
+    public void NegTest1() { //Negative Zahlen testen
+        GermanTranslator translator = new GermanTranslator();
+
+        assertEquals("Übersetzung der Zahl -1 nicht möglich (1.0)" , translator.translateNumber(-1));
+    }
+    @Test
+    public void Neg2Test() { //Positive Zahlen außerhalb des Definitionsbereichs testen
+        GermanTranslator translator = new GermanTranslator();
+
+        assertEquals("Übersetzung der Zahl 11 nicht möglich (1.0)" , translator.translateNumber(11));
+    }
+
 
 }
